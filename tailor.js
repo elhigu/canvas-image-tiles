7364d77ebd1b919f87c714ef0a94051ed7fd5b27
Mikael Lepistö <mikael.lepisto@vincit.fi>
2015-05-12 15:34:52 +0300

    Initial version of tile creator tested with firefox and chrome.

---
+++ tailor.js
@@ -0,0 +1,114 @@
+var fileInput = document.getElementById('file-input');
+var canvas = document.getElementById('tile-out');
+var fpsCounter = document.getElementById('fps');
+var imageInfo = document.getElementById('image-info');
+
+var sourceReady = false;
+var sourceCanvas = document.createElement('canvas');
+var previewCanvas = document.getElementById('preview');
+
+/**
+ * Load image data from file and create original sized canvas from it for
+ * generating tiles and lower resolution preview for showing it in screen.
+ * 
+ * Image element could be used directly as a source for creating tiles, but
+ * e.g. Chrome did automatically store image in GPU memory, which caused 
+ * reading image for creating tiles with drawImage to be really slow.
+ */
+function handleFileSelect(evt) {
+  previewCanvas.width = 200;
+  previewCanvas.height = 200;
+  var ctx = previewCanvas.getContext('2d');
+  ctx.fillStyle = "blue";
+  ctx.font = "bold 16px Arial";
+  ctx.fillText("Loading....", 0, 100);
+
+	var files = evt.target.files;
+	var output = [];
+	for (var i = 0, f; f = files[i]; i++) {
+    sourceReady = false;
+    console.log(f);
+    var reader = new FileReader();
+    reader.onloadend = function (evt) {
+      if (evt.target.readyState == FileReader.DONE) {
+        // load data to image element, to be able to create preview and
+        // original image canvases
+        var newImage = document.createElement('img');
+
+        newImage.onload = function() {
+          sourceReady = true;
+          sourceCanvas.width = newImage.naturalWidth;
+          sourceCanvas.height = newImage.naturalHeight;
+
+          // copy image to canvas, to prevent img tag being stored in GPU memory
+          // which did slow down slicing hugely on chrome 
+          // (or we could write webgl slicing for that case)...
+          var sourceCtx = sourceCanvas.getContext('2d');
+          sourceCtx.drawImage(newImage, 0, 0);
+
+          // create smaller preview canvas
+          var aspectRatio = sourceCanvas.width / sourceCanvas.height;
+          var isHeightBigger = aspectRatio < 1;
+          if (isHeightBigger) {
+            previewCanvas.height = 2048;
+            previewCanvas.width = aspectRatio*previewCanvas.height;
+          } else {
+            previewCanvas.width = 2048;
+            previewCanvas.height = previewCanvas.width/aspectRatio;
+          }
+          var previewCtx = previewCanvas.getContext('2d');
+          previewCtx.drawImage(sourceCanvas, 
+            0,0,sourceCanvas.width,sourceCanvas.height,
+            0,0,previewCanvas.width,previewCanvas.height
+          );
+
+          imageInfo.textContent = sourceCanvas.width + "x" + sourceCanvas.height;
+        };
+        newImage.src = evt.target.result;
+      }
+    };
+    reader.readAsDataURL(f);
+  }
+}
+fileInput.addEventListener('change', handleFileSelect, false);
+
+/**
+ * Sample tile from source image on every animationFrame.
+ */
+var frameCount = 0;
+function slicePieceToCanvas() {
+  if (sourceReady) {
+    // clear canvas and old transformations
+    var context = canvas.getContext('2d');
+    context.save();
+
+    // scale, rotate, position for receiving canvas
+    var scale = Math.random()*2;
+    context.scale(scale,scale)
+    context.rotate(Math.random()*Math.PI*2);
+    context.translate(-sourceCanvas.width*Math.random(), -sourceCanvas.height*Math.random());
+
+    context.drawImage(sourceCanvas, 0, 0);
+    frameCount++;
+    context.restore();
+  }
+  requestAnimationFrame(slicePieceToCanvas);    
+}
+// start render loop on next animation frame
+requestAnimationFrame(slicePieceToCanvas);
+
+/**
+ * Update fps counter every 2 secs if image data is ready.
+ */
+var previousFrameCount = 0;
+var previousMilliseconds = new Date().getTime();
+function updateFps() {
+  var currentMs = new Date().getTime();
+  var framesPassed = frameCount - previousFrameCount;
+  var millisecondsPassed = currentMs - previousMilliseconds;
+  var fps = framesPassed/(millisecondsPassed/1000);
+  fpsCounter.textContent = fps;
+  previousFrameCount = frameCount;
+  previousMilliseconds = currentMs;
+}
+setInterval(updateFps, 2000);
