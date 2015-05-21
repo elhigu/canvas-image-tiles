1c339862a9e7b848998357add3986bd1496a19f6
Mikael Lepistö <mikael.lepisto@vincit.fi>
2015-05-22 01:29:27 +0300

    Fixed demo page to work with grid.

--- javascripts/tailor.js
+++ javascripts/tailor.js
@@ -41,6 +41,7 @@ var orientation = {
         console.log("Updating transform...", this.getTransform());
         this.waitForUpdate = false;
         previewLocator.style.transform = this.getTransform();
+        requestAnimationFrame(slicePieceToCanvas);
       }.bind(this));
     }
   },
@@ -70,18 +71,18 @@ var orientation = {
  * Load image data from file and create original sized canvas from it for
  * generating tiles and lower resolution preview for showing it in screen.
  *
- * Image element could be used directly as a source for creating tiles, but
+ * Image element could be used directly as a source for craeating tiles, but
  * e.g. Chrome did automatically store image in GPU memory, which caused
  * reading image for creating tiles with drawImage to be really slow.
  */
-var gridCenterEl = document.querySelector('#tile-selector-grid .cell:nth-child(28)');
+var gridCenterEl = document.querySelector('#tile-selector-grid .cell:nth-child(36)');
 function handleFileSelect(evt) {
-  previewCanvas.width = 200;
-  previewCanvas.height = 200;
+  previewCanvas.width = 512;
+  previewCanvas.height = 512;
   var ctx = previewCanvas.getContext('2d');
   ctx.fillStyle = "blue";
-  ctx.font = "bold 16px Arial";
-  ctx.fillText("Loading....", 0, 100);
+  ctx.font = "bold 28px Arial";
+  ctx.fillText("Behold...", 128, 256);
 
   var files = evt.target.files;
   var output = [];
@@ -101,7 +102,7 @@ function handleFileSelect(evt) {
           sourceCanvas.height = newImage.naturalHeight;
 
           // copy image to canvas, to prevent img tag being stored in GPU memory
-          // which did slow down slicing hugely on chrome 
+          // which did slow down slicing hugely on chrome
           // (or we could write webgl slicing for that case)...
           var sourceCtx = sourceCanvas.getContext('2d');
           sourceCtx.drawImage(newImage, 0, 0);
@@ -159,10 +160,7 @@ function slicePieceToCanvas() {
     context.restore();
   }
   frameCount++;
-  requestAnimationFrame(slicePieceToCanvas);
 }
-// start render loop on next animation frame
-requestAnimationFrame(slicePieceToCanvas);
 
 /**
  * Update fps counter every 2 secs if image data is ready.
@@ -194,6 +192,7 @@ function onGridCellClick(event) {
   previouslySelected = el;
   orientation.selectedGridPos = { x: el.offsetLeft, y: el.offsetTop };
   console.log("Selected grid position", event, el.offsetTop, el.offsetLeft);
+  requestAnimationFrame(slicePieceToCanvas);
 }
 selectorGrid.addEventListener('click', onGridCellClick, false);
 
